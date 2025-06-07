abf710277f2edf3866975671d50f592d98750c38
Pasha <pashaie@gmail.com>
2025-06-07 16:25:30 +0330

    farsi/persian locale

---
+++ src/locales/fa/index.ts
@@ -0,0 +1,22 @@
+import menu from './menu.json';
+import layout from './layout.json';
+import login from './login.json';
+import grid from '@/pages/grid/locales/en/grid.json';
+import permission from '@/pages/permission/locales/en/permission.json';
+import router from '@/pages/router/locales/en/router.json';
+import tablePage from '@/pages/tablePage/locales/en/tablePage.json';
+import error from './error.json';
+
+const en = {
+  menu,
+  layout,
+  login,
+  grid,
+  permission,
+  router,
+  tablePage,
+  error,
+};
+
+export default en;
+
